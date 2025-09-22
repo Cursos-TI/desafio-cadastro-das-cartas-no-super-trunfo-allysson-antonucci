adc5d864ea2f08135685e5521f232ed258053a63
Allysson <allysson.antonucci.dev@gmail.com>
2025-09-22 02:30:56 +0000

    Desafio Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
     // Variáveis das Cartas 1 e 2
     char estado1, codigo1[4], cidade1[40], estado2, codigo2[4], cidade2[40];
     int populacao1, numeroPontosTuristicos1, populacao2, numeroPontosTuristicos2;
-    float area1, pib1, densidadePopulacional1, pibPerCapta1, area2, pib2, densidadePopulacional2, pibPerCapta2;
+    float area1, pib1, densidadePopulacional1, pibPerCapta1, superPoder1, area2, pib2, densidadePopulacional2, pibPerCapta2, superPoder2;
     
     // Coletando os dados da Carta 1
 
@@ -71,7 +71,13 @@ int main() {
     pibPerCapta1 = pib1*1000000000/populacao1; // Conversão do PIB para bilhão
     
     pibPerCapta2 = pib2*1000000000/populacao2;
+        
+    // Calculando o super poder (Soma de todos os atributos numéricos e o inverso da densidade populacional)
+    
+    superPoder1 = (float) populacao1 + (float) numeroPontosTuristicos1 + area1 + pib1 + (1/densidadePopulacional1) + pibPerCapta1;
 
+    superPoder2 = (float) populacao2 + (float) numeroPontosTuristicos2 + area2 + pib2 + (1/densidadePopulacional2) + pibPerCapta2;
+    
     // Exibindo os dados coletados da Carta 1
     printf("\nCarta 1\n");
     printf("Estado: %c\n", estado1);
@@ -95,7 +101,19 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos2);
     printf("Densidade populacional: %.2f hab/km²\n", densidadePopulacional2);
-    printf("PIB per Capta: R$%.2f\n", pibPerCapta2);
+    printf("PIB per Capta: R$%.2f\n\n", pibPerCapta2);
 
+
+    // Exibindo a comparação dos atributos numéricos das Cartas
+    printf("Comparação de Cartas:\n");
+    printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+    printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
+    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
+    printf("Pontos turísticos: Carta 1 venceu (%d)\n", numeroPontosTuristicos1 > numeroPontosTuristicos2);
+    printf("Densidade populacional: Carta 1 venceu (%d)\n", densidadePopulacional1 < densidadePopulacional2); // Como é o inverso, o menor vence 
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibPerCapta1 > pibPerCapta2);
+    printf("Super Poder: Carta 1 venceu (%d)\n", superPoder1 > superPoder2);
+    
     return 0;
 }
+
